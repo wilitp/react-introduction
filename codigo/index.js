d06871a413e642fa697517cb50b2f612f916ba4d
wilitp <guilledeipola@gmail.com>
2020-12-06 22:21:15 -0300

    Agregar código del tutorial

---
+++ codigo/index.js
@@ -0,0 +1,8 @@
+// const div = React.createElement('div', {
+//   children: React.createElement('p', {children: "Hola mundo!"})
+// })
+
+const div = <div><p>Hola Mundo!</p></div>
+
+
+ReactDOM.render(div, document.body)
